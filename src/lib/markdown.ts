5449fd0b80a827cce83a48e9a228340ab3a60f8f
BowlofBryce <93360109+BowlofBryce@users.noreply.github.com>
2026-01-01 14:59:13 -0700

    Added markdown.ts

---
+++ src/lib/markdown.ts
@@ -0,0 +1,12 @@
+export function parseMarkdown(text: string): string {
+  if (!text) return '';
+
+  return text
+    .replace(/\*\*\*(.*?)\*\*\*/g, '<strong><em>$1</em></strong>')
+    .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
+    .replace(/\*(.*?)\*/g, '<em>$1</em>')
+    .replace(/__(.*?)__/g, '<strong>$1</strong>')
+    .replace(/_(.*?)_/g, '<em>$1</em>')
+    .replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2" target="_blank" rel="noopener noreferrer" class="text-pink-400 hover:text-pink-300 underline">$1</a>')
+    .replace(/\n/g, '<br />');
+}
